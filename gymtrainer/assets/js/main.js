27c9f5c6416126dc654dbd40bc190b5b4c220648
AHAMD TARIQUE <ahmadtarique2004@gmail.com>
2024-12-27 11:10:47 +0530

    Add files via upload
    changes to the previous website

---
+++ gymtrainer/assets/js/main.js
@@ -0,0 +1,115 @@
+/*
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDEOpXJ9PIhFOteQJqTxgBgnaxPiw-IeMI",
+    authDomain: "gym-trainer-aman.firebaseapp.com",
+    databaseURL: "https://gym-trainer-aman-default-rtdb.firebaseio.com",
+    projectId: "gym-trainer-aman",
+    storageBucket: "gym-trainer-aman.firebasestorage.app",
+    messagingSenderId: "741007212452",
+    appId: "1:741007212452:web:98f56679e6020cd4538b31"
+  };
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var contactFormDB = firebase.database().ref("gym-trainer-aman");
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var emailid = getElementVal("emailid");
+  var msgContent = getElementVal("msgContent");
+  var msgContent1=getElementVal("msgcontent1");
+
+  saveMessages(name, emailid, msgContent,msgContent1);
+
+
+  //   reset the form
+  document.getElementById("contactForm").reset();
+}
+
+const saveMessages = (name, emailid, msgContent,msgContent1) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    name: name,
+    emailid: emailid,
+    msgContent: msgContent,
+	password: msgContent1
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '1281px',  '1680px' ],
+			normal:    [ '981px',   '1280px' ],
+			narrow:    [ '841px',   '980px'  ],
+			narrower:  [ '737px',   '840px'  ],
+			mobile:    [ '481px',   '736px'  ],
+			mobilep:   [ null,      '480px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			offsetY: -15,
+			hoverDelay: 0,
+			alignment: 'center'
+		});
+
+	// Nav.
+
+		// Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
